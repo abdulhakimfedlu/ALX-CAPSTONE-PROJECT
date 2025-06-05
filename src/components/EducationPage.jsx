7139840db094c9ccf15f4651dae3beb7d8f95998
Abdulhakim Fedlu <155722598+abdulhakimfedlu@users.noreply.github.com>
2025-06-05 22:19:47 +0300

    Update EducationPage.jsx

--- src/components/EducationPage.jsx
+++ src/components/EducationPage.jsx
@@ -4,7 +4,6 @@ const EducationPage = () => {
   const [activeSection, setActiveSection] = useState('basics');
   
   const sections = {
-
     
     basics: {
       title: 'Understanding Exchange Rates',
