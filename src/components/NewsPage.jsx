6c5e7ca7eed3b5edba487e2477a445ea3a597203
Abdulhakim Fedlu <155722598+abdulhakimfedlu@users.noreply.github.com>
2025-06-08 22:01:18 +0300

    Update NewsPage.jsx

--- src/components/NewsPage.jsx
+++ src/components/NewsPage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
+
 const NewsPage = () => {
   const [news, setNews] = useState([]);
   const [trendingPairs, setTrendingPairs] = useState([]);
